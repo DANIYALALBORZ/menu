e4f78d713bff0f97d364d83e78c1c51c3eaab127
Danial Alborz <danialalborz3@gmail.com>
2024-10-06 14:02:46 -0700

    design menu icon

--- assets/js/danial.js
+++ assets/js/danial.js
@@ -1,5 +1,7 @@
 let toggle = document.querySelector('#toggle');
+let overlay = document.querySelector('.overlay');
 
 toggle.addEventListener('click', function (e) {
     toggle.classList.toggle('active');
+    overlay.classList.toggle('active');
 })
